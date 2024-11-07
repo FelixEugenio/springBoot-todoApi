9e08bcd50553d004087cd3850152ad9bdd0b92cb
Felix Eugenio Mavila <felixmacarenco@gmail.com>
2024-11-07 03:10:41 +0000

    feat: create userRepository interface

--- src/main/java/com/mavila/task_app/repositories/userRepository.java
+++ src/main/java/com/mavila/task_app/repositories/userRepository.java
@@ -1,5 +1,9 @@
 package com.mavila.task_app.repositories;
 
-public class userRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-}
+import com.mavila.task_app.models.User;
+
+@Repository
+public interface userRepository extends JpaRepository<User, Long>{}
